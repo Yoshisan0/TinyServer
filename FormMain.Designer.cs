20db6d63427e72bd087dfb24e367290616e9afe2
Yoshisan0 <yoshisan_t@asterios.biz>
2025-05-28 20:31:33 +0900

    ログを正常に出力するようにしました ログをコピーできるようにしました

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,24 +29,34 @@
         private void InitializeComponent()
         {
             this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listView
             // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
             this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.GridLines = true;
             this.listView.HideSelection = false;
             this.listView.Location = new System.Drawing.Point(0, 0);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(384, 261);
+            this.listView.Size = new System.Drawing.Size(784, 261);
             this.listView.TabIndex = 0;
             this.listView.UseCompatibleStateImageBehavior = false;
-            this.listView.View = System.Windows.Forms.View.List;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Log";
+            this.columnHeader1.Width = 750;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ClientSize = new System.Drawing.Size(784, 261);
             this.Controls.Add(this.listView);
             this.Name = "FormMain";
             this.Text = "TinyServer";
@@ -59,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -1,9 +1,13 @@
 ﻿using MiniJSON;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Runtime.Remoting.Contexts;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TinyServer
 {
@@ -13,6 +17,7 @@ namespace TinyServer
         private string m_Lang = "en-US";
         private string m_DocumentRoot = "./wwwroot";
         private HttpListener m_Listener = null;
+        private Color m_DarkYellow = Color.FromArgb(128, 128, 0);
 
         public FormMain()
         {
@@ -94,6 +99,12 @@ namespace TinyServer
         /// </summary>
         private async void StartHttpServer()
         {
+            string ipAddress = "x.x.x.x";
+            string timeStamp = "xxxx-xx-xx xx:xx:xx";
+            string method = "x";
+            string path = "x";
+            ListViewItem item;
+
             try
             {
                 string url = string.Format("http://localhost:{0}/", m_Port);
@@ -101,14 +112,24 @@ namespace TinyServer
                 m_Listener = new HttpListener();
                 m_Listener.Prefixes.Add(url);
                 m_Listener.Start();
-                listView.Items.Add("Server start. " + url);
+
+                timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                item = listView.Items.Add($"[INFO] {timeStamp} - Server start. {url}");
+                item.ForeColor = Color.DarkGreen;
 
                 string rootPath = Path.GetFullPath(m_DocumentRoot);
                 while (true)
                 {
+                    ipAddress = "x.x.x.x";
+                    method = "x";
+                    path = "x";
+
                     var context = await m_Listener.GetContextAsync();
                     string urlPath = context.Request.Url.LocalPath.TrimStart('/');
                     string requestedPath = Path.Combine(m_DocumentRoot, urlPath);
+                    ipAddress = context.Request.RemoteEndPoint?.Address.ToString() ?? "unknown";
+                    method = context.Request.HttpMethod;
+                    path = context.Request.Url.LocalPath;
 
                     // デフォルトで index.html を返す
                     if (string.IsNullOrEmpty(urlPath))
@@ -126,33 +147,65 @@ namespace TinyServer
                             writer.Write("403 Forbidden");
                         }
 
+                        timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                        item = listView.Items.Add($"[WARN] {timeStamp} - 403 Forbidden - Method: {method}, Path: {path}, IP: {ipAddress}");
+                        item.ForeColor = m_DarkYellow;
+
                         context.Response.OutputStream.Close();
                         continue;
                     }
 
                     // ファイルの存在チェックとレスポンス送信
-                    if (File.Exists(fullPath))
-                    {
-                        byte[] data = File.ReadAllBytes(fullPath);
-                        context.Response.ContentType = Tool.GetContentTypeFromPath(fullPath);
-                        context.Response.ContentLength64 = data.Length;
-                        await context.Response.OutputStream.WriteAsync(data, 0, data.Length);
-                    }
-                    else
+                    if (!File.Exists(fullPath))
                     {
                         context.Response.StatusCode = 404;
                         using (StreamWriter writer = new StreamWriter(context.Response.OutputStream))
                         {
                             writer.Write("404 Not Found");
                         }
+
+                        timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                        item = listView.Items.Add($"[WARN] {timeStamp} - 404 Not Found - Method: {method}, Path: {path}, IP: {ipAddress}");
+                        item.ForeColor = m_DarkYellow;
+
+                        context.Response.OutputStream.Close();
+                        continue;
                     }
+
+                    byte[] data = File.ReadAllBytes(fullPath);
+                    context.Response.ContentType = Tool.GetContentTypeFromPath(fullPath);
+                    context.Response.ContentLength64 = data.Length;
+                    await context.Response.OutputStream.WriteAsync(data, 0, data.Length);
+
+                    timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    item = listView.Items.Add($"[INFO] {timeStamp} - 200 OK - Method: {method}, Path: {path}, IP: {ipAddress}");
+                    item.ForeColor = Color.DarkGreen;
 
                     context.Response.OutputStream.Close();
                 }
             }
             catch (Exception err)
             {
-                listView.Items.Add("Server error. " + err.Message);
+                timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                item = listView.Items.Add($"[ERRO] {timeStamp} - Exception: {err.Message} - Method: {method}, Path: {path}, IP: {ipAddress}");
+                item.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void listView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                if (listView.SelectedItems.Count == 0) return;
+
+                // 選択されたアイテムのテキストを1行ずつ連結
+                var lines = listView.SelectedItems
+                    .Cast<ListViewItem>()
+                    .Select(item => item.Text);
+
+                string text = string.Join(Environment.NewLine, lines);
+
+                Clipboard.SetText(text);
             }
         }
     }
